eb7c1fd4175435ac4f25f82f69df8e04246ff60c
MariaLBovin <maria.bovin@gmail.com>
2023-02-23 11:49:52 +0100

    textarray added

---
+++ src/ts/textarray.ts
@@ -0,0 +1,31 @@
+export const textArray = [{
+    obstacle: 'Hopphinder',
+    description:`Det vanligaste hindret som finns inom agilityn är hopphinder. På dessa kan man hoppa rakt fram, man kan svänga tajt till höger eller vänster, man kan ta hopphindret från utsidan, man kan 
+hoppa över hindret som ett s och mycket mer. Höjden på hopphindren varierar beroende på hur stor hund du har, de kan vara allt från 10 cm höga till 60 cm höga. Förutom vanliga hopphinder 
+finns det tre specialhopphinder. Dessa är långhoppet, muren och däcket.`
+}, 
+{
+    obstacle: 'Tunnel',
+    description: `Det näst vanligaste hindret på en agilitybana är tunneln. Den finns (oftast) i tre längder, 3 meter, 4 meter och 6 meter. Den kan ligga helt rakt eller böjd. Ibland ska hunden ta ingången som är närmast dig, ibland den längst bort från dig. När man tränar med tunnlar är det viktigt att tänka på att de ska vara ordentligt förankrade så att inte hunden riskerar att skada sig eller bli rädd om tunneln rör sig.
+`
+},
+{
+    obstacle: 'A-hinder',
+    description: `A-hindret är ett sk kontaktfältshinder. Det betyder att det finns två fält längst ner på sidorna av A-hindret som är en annan färg. Detta fält måste hunden sätta tassarna på innan den lämnar hindret. 
+A-hindret är ett högt hinder och det kräver god fysik hos hunden för att klara av att springa uppför och bromsa på vägen ner.`
+},
+{
+    obstacle: 'Gungan',
+    description: `Kallas även för vippen. Gungan är en klassisk gungbräda där hunden vippar hindret med sin tyngd. Även här finns det kontaktfält ytterst på båda sidor som hunden ska röra vid innan den får kliva av.
+Gungan kan vara ett hinder som många hundar blir rädda för så låt inte din hund springa upp utan att du är där och kan ta emot.`
+},
+{
+    obstacle: 'Balansbom',
+    description: `Det här är det tredje och sista kontaktfältshindret och är utformat som en lång, upphöjd balansbräda som hunden ska springa i full fart över. Precis som med gungan kan vissa hundar
+tycka att balansen är obehaglig pga sin höjd så se till att din hund inte springer upp om inte du har tillåtit det.`
+},
+{
+    obstacle: 'Slalom',
+    description: `Slalomet består av 12 slalompinnar som hunden ska springa igenom. `
+}  
+]
