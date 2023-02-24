87eb1b8cb7859265cfa1f42cafd4858422524477
MariaLBovin <maria.bovin@gmail.com>
2023-02-24 14:03:14 +0100

    do-functions added

--- src/ts/textslide.ts
+++ src/ts/textslide.ts
@@ -1,15 +1,18 @@
 import { textArray } from "./arrays/textarray";
 
-const textPlacement : HTMLElement = document.querySelector('.section__about') as HTMLElement;
+const textPlacement : HTMLElement = document.querySelector('.section__do') as HTMLElement;
 
 function createAboutText () {
     for (let i = 0; i < textArray.length; i++){
         textPlacement.innerHTML += `
-        <div class = "obstc$[i]}">
-        <button class="section__about-btn obstc${[i]}">${textArray[i].obstacle}</button>
+        <div class="obstacle">
+        <div class = "obstc${[i]} obstacle_name">
+        <h2 class="section__about-btn obstc${[i]}">${textArray[i].obstacle}</h2>
         </div>
-        <div class="section__about-article obstc${[i]}">${textArray[i].description}</div>
+        <div class="section__about-article text-div${[i]}   obstc${[i]} ">${textArray[i].description}</div>
+        </div>
         `
     }
 }
 createAboutText();
+
